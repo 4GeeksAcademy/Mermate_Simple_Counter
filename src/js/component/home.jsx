ce89b332b20caa4156968096f1f997648452e648
Mermate <138124998+Mermate@users.noreply.github.com>
2023-11-09 01:28:06 +0000

    First Commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,10 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import PropTypes from 'prop-types';
-
-import propTypes from "prop-types";
-
-
 
 //include images into your bundle
 //import rigoImage from "../../img/rigo-baby.jpg";
@@ -25,29 +20,19 @@ import propTypes from "prop-types";
 					<div className=" digit two">{props.digitTwo}</div> 
 					<div className=" digit one">{props.digitOne}</div>
 					</div>
-				
 				);
 			};
 
-			SimpleCounter.propTypes = {
-				digitSix:propTypes.number,
-				digitFive:propTypes.number,
-				digitFour:PropTypes.number,
-				digitThree:PropTypes.number,
-				digitTwo: PropTypes.number,
-				digitOne: PropTypes.number
-		};
-
-	let counter = 0;
-	setInterval(function(){
-		const six = Math.floor(counter/100000) % 10;
-        const five = Math.floor(counter/10000) % 10;
-		const four = Math.floor(counter/1000) % 10;
-		const three = Math.floor(counter/100) % 10;
-		const two = Math.floor(counter/10) % 10;
-		const one = Math.floor(counter/1) % 10;
-		console.log(four, three, two, one);
-		counter++;
+		let counter = 0;
+			setInterval(function(){
+			const six = Math.floor(counter/100000) % 10;
+			const five = Math.floor(counter/10000) % 10;
+			const four = Math.floor(counter/1000) % 10;
+			const three = Math.floor(counter/100) % 10;
+			const two = Math.floor(counter/10) % 10;
+			const one = Math.floor(counter/1) % 10;
+			console.log(four, three, two, one);
+			counter++;
 
     ReactDOM.render(
         <SimpleCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} digitFive={five} digitSix={six}/>,
